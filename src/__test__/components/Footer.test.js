ab7734d89dc83a360a5cda35163b5a5e68e62f20
Oscar Barajas <oscar@dragons.mx>
2019-11-18 11:20:59 -0500

    This is where it all begins...

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,12 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+
   test('Render del componente Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
