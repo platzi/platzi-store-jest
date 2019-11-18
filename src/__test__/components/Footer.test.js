3deef1fe10faea01ebe0ff5ec0d06c686ab5d29a
Oscar Barajas <oscar@dragons.mx>
2019-11-18 10:42:41 -0500

    Reinventing the wheel. Again

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer />', () => {
+  test('Render del componente Footer', () => {
+    const footer = mount(<Footer />);
+    expect(footer.length).toEqual(1);
+  });
+});
