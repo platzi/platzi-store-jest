d3a9c2f67e0907f5295601f8a57cf5e98e28dc01
Oscar Barajas <oscar@dragons.mx>
2019-11-18 12:41:26 -0500

    Lock S-foils in attack position

---
+++ src/__test__/reducers/reducers.test.js
@@ -0,0 +1,24 @@
+import reducer from '../../reducers';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Reducers', () => {
+  test('Retornar initial State', () => {
+    expect(reducer({}, '')).toEqual({});
+  });
+  test('ADD_TO_CART', () => {
+    const initialState = {
+      cart: [],
+    };
+    const payload = ProductMock;
+    const action = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    const expected = {
+      cart: [
+        ProductMock,
+      ],
+    };
+    expect(reducer(initialState, action)).toEqual(expected);
+  });
+});
